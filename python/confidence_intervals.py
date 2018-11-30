511e3b8801953c30ff4eaf717af0527982c12ff7
Satyarth Mishra Sharma <mail@satyarth.me>
2018-11-30 10:30:42 +0300

    Make z a tunable parameter in confidence intervals

--- python/confidence_intervals.py
+++ python/confidence_intervals.py
@@ -1,22 +1,21 @@
 from copy import deepcopy
 import numpy as np
 
-def confidence_intervals(df_test, sample_name='abortus'):
-    z = 3
-    predictions = deepcopy(df_test[sample_name + '^GT'])
-    idx_hetero = (df_test[sample_name + '^GT'] == 1)
+def confidence_intervals(df_test, ab_name='abortus', mo_name="mother", z=3):
+    predictions = deepcopy(df_test[ab_name + '^GT'])
+    idx_hetero = (df_test[ab_name + '^GT'] == 1)
     contaminations = df_test['contamination'].values
 
-    lower_bound = contaminations - z*np.sqrt(contaminations*(1 - contaminations)/df_test[sample_name + '^DP'].values)
-    upper_bound = contaminations + z*np.sqrt(contaminations*(1 - contaminations)/df_test[sample_name + '^DP'].values)
+    lower_bound = contaminations - z*np.sqrt(contaminations*(1 - contaminations)/df_test[ab_name + '^DP'].values)
+    upper_bound = contaminations + z*np.sqrt(contaminations*(1 - contaminations)/df_test[ab_name + '^DP'].values)
 
-    idx_0 = idx_hetero & (df_test[sample_name + '^AD0']/df_test[sample_name + '^AD1'] > df_test['mother^AD0']/df_test['mother^AD1'])
-    mo_share = (2*df_test[sample_name + '^AD1']/df_test[sample_name + '^DP'])
+    idx_0 = idx_hetero & (df_test[ab_name + '^AD0']/df_test[ab_name + '^AD1'] > df_test[mo_name + '^AD0']/df_test[mo_name + '^AD1'])
+    mo_share = (2*df_test[ab_name + '^AD1']/df_test[ab_name + '^DP'])
     idx_0_confirmed = (mo_share > lower_bound) & (mo_share < upper_bound) & idx_0
     predictions.loc[idx_0_confirmed] = 0
 
-    idx_1 = idx_hetero & (df_test[sample_name + '^AD0']/df_test[sample_name + '^AD1'] < df_test['mother^AD0']/df_test['mother^AD1'])
-    mo_share = (2*df_test[sample_name + '^AD0']/df_test[sample_name + '^DP'])
+    idx_1 = idx_hetero & (df_test[ab_name + '^AD0']/df_test[ab_name + '^AD1'] < df_test[mo_name + '^AD0']/df_test[mo_name + '^AD1'])
+    mo_share = (2*df_test[ab_name + '^AD0']/df_test[ab_name + '^DP'])
     idx_1_confirmed = (mo_share > lower_bound) & (mo_share < upper_bound) & idx_1
     predictions.loc[idx_1_confirmed] = 2
 
